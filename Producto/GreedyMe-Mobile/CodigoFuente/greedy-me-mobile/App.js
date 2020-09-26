28e5066a046e844c7b479616c04370fa4202b566
YazimelNiclis <yazimel_niclis@hotmail.com>
2020-09-26 18:46:26 -0300

    a

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/App.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './pantallas/Home';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
   );
 }
 
@@ -17,5 +18,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  flex: {
+    flex: 1,
   },
 });
