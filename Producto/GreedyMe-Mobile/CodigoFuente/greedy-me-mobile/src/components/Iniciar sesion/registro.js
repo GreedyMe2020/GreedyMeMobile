edad7348e12ba10a5c62ad49131263dafb3241db
JuampiZablosky <44734692+JuampiZablosky@users.noreply.github.com>
2020-10-05 15:54:55 -0300

    agrego registro de usuario

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/Iniciar sesion/registro.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/Iniciar sesion/registro.js
@@ -8,35 +8,111 @@ import {
   Text,
   TouchableWithoutFeedback,
 } from 'react-native';
-import SafeAreaView from 'react-native-safe-area-view';
-import { Form, TextValidator } from 'react-native-validator-form';
+import { Button, TextInput } from 'react-native-paper';
+import { connect } from 'react-redux';
 
 function Registro(props) {
-  const [formData, setFormData] = React.useState({
-    nombre: '',
-    apellido: '',
-    email: '',
-    password: '',
-    repeticion: '',
-  });
+  //Estados para cada uno de los inputs del formulario de registro
+  const [nombre, setNombre] = React.useState(null);
+  const [apellido, setApellido] = React.useState(null);
+  const [email, setEmail] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  const [repetido, setRepetido] = React.useState(null);
 
-  const handleChange = (event) => {
-    formData[event.target.name] = event.target.value;
-    setFormData({ ...formData });
+  //Estados para los errores de cada uno de los inputs del fomr de registro
+  const [errorNombre, setErrorNombre] = React.useState('');
+  const [errorApellido, setErrorApellido] = React.useState('');
+  const [errorEmail, setErrorEmail] = React.useState('');
+  const [errorContraseñaNueva, setErrorContraseñaNueva] = React.useState('');
+  const [errorContraseñaRepe, setErrorContraseñaRepe] = React.useState('');
+  //Estado para los errores de envio de los formularios
+  const [mensajeError, setMensajeError] = React.useState('');
+  //Estado para saber si las contraseñas son iguales
+  const [esDistinta, setEsDistinta] = React.useState('');
+
+  const handleChangeNombre = (nombre) => {
+    setNombre(nombre);
+    console.log(nombre);
+  };
+  const handleChangeApellido = (apellido) => {
+    setApellido(apellido);
+  };
+  const handleChangeEmail = (email) => {
+    setEmail(email);
+  };
+  const handleChangePassword = (password) => {
+    setPassword(password);
+  };
+
+  const handleChangePasswordRep = (passwordRep) => {
+    setRepetido(passwordRep);
   };
 
   const handleSubmit = () => {
-    console.log(formData);
+    if (
+      nombre === '' ||
+      apellido === '' ||
+      email === '' ||
+      password === '' ||
+      repetido === '' ||
+      nombre === null ||
+      apellido === null ||
+      email === null ||
+      password === null ||
+      repetido === null
+    ) {
+      setMensajeError('Todos campos deben ser completados');
+    } else {
+      setMensajeError('');
+      if (password === repetido) {
+        setEsDistinta('');
+        //Agregar funcion para registrar nuevo usuario
+      } else {
+        setEsDistinta('Las contraseñas no son iguales');
+      }
+    }
   };
 
-  /* const handleChangeEmail = (email) => {
-    setEmail(email);
-  };
+  //Funciones para manejar los errores individuales de cada input
+  const nombreValidator = React.useEffect(() => {
+    if (nombre === '') {
+      setErrorNombre('* Este campo no puede estar vacio');
+    } else {
+      setErrorNombre('');
+    }
+  }, [nombre]);
 
-  const handleChangePassword = (password) => {
-    setPassword(password);
-  }; */
-  const form = React.createRef();
+  const apellidoValidator = React.useEffect(() => {
+    if (apellido === '') {
+      setErrorApellido('* Este campo no puede estar vacio');
+    } else {
+      setErrorApellido('');
+    }
+  }, [apellido]);
+
+  const emailValidator = React.useEffect(() => {
+    if (email === '') {
+      setErrorEmail('* Este campo no puede estar vacio');
+    } else {
+      setErrorEmail('');
+    }
+  }, [email]);
+
+  const passValidatorNueva = React.useEffect(() => {
+    if (password === '') {
+      setErrorContraseñaNueva('* Este campo no puede estar vacio');
+    } else {
+      setErrorContraseñaNueva('');
+    }
+  }, [password]);
+
+  const passValidatorRepetida = React.useEffect(() => {
+    if (repetido === '') {
+      setErrorContraseñaRepe('* Este campo no puede estar vacio');
+    } else {
+      setErrorContraseñaRepe('');
+    }
+  }, [repetido]);
 
   return (
     <KeyboardAvoidingView
@@ -47,106 +123,107 @@ function Registro(props) {
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-          <Form ref={form} onSubmit={handleSubmit}>
-            <TextValidator
-              style={styles.inputEmailPass}
-              placeholderTextColor="#000000"
-              required
-              label="Nombre"
-              name="nombre"
-              validators={['required']}
-              errorMessages={['* Este campo es obligatorio']}
-              placeholder="Nombre"
-              type="text"
-              value={formData.nombre}
-              onChangeText={handleChange}
-              errorStyle={{
-                container: { top: 0, left: 20, position: 'relative' },
-                text: { color: 'red' },
-                underlineValidColor: '#1E1B4D',
-                underlineInvalidColor: 'red',
+          <TextInput
+            style={styles.inputEmailPass}
+            mode="flat"
+            label="Nombre"
+            name="nombre"
+            required
+            underlineColor="#76B39D"
+            value={nombre}
+            onBlur={() => {
+              nombreValidator;
+            }}
+            onChangeText={handleChangeNombre}
+            error={errorNombre}
+          />
+          <Text style={styles.errorPass}>{errorNombre}</Text>
+
+          <TextInput
+            style={styles.inputEmailPass}
+            mode="flat"
+            label="Apellido"
+            name="apellido"
+            required
+            underlineColor="#76B39D"
+            onBlur={() => {
+              apellidoValidator;
+            }}
+            value={apellido}
+            onChangeText={handleChangeApellido}
+            error={errorApellido}
+          />
+          <Text style={styles.errorPass}>{errorApellido}</Text>
+
+          <TextInput
+            style={styles.inputEmailPass}
+            mode="flat"
+            label="Email"
+            required
+            disabled
+            underlineColor="#76B39D"
+            onBlur={() => {
+              emailValidator;
+            }}
+            value={email}
+            onChangeText={handleChangeEmail}
+            error={errorEmail}
+          />
+          <Text style={styles.errorPass}>{errorEmail}</Text>
+
+          <TextInput
+            style={styles.inputEmailPass}
+            mode="flat"
+            label="Contraseña"
+            required
+            disabled
+            underlineColor="#76B39D"
+            onBlur={() => {
+              passValidatorNueva;
+            }}
+            value={password}
+            onChangeText={handleChangePassword}
+            secureTextEntry={true}
+            error={errorContraseñaNueva}
+          />
+          <Text style={styles.errorPass}>{errorContraseñaNueva}</Text>
+
+          <TextInput
+            style={styles.inputEmailPass}
+            mode="flat"
+            label="Contraseña"
+            required
+            disabled
+            underlineColor="#76B39D"
+            onBlur={() => {
+              passValidatorRepetida;
+            }}
+            value={repetido}
+            secureTextEntry={true}
+            onChangeText={handleChangePasswordRep}
+            error={errorContraseñaRepe}
+          />
+          <Text style={styles.errorPass}>{errorContraseñaRepe}</Text>
+
+          <View style={styles.contenedorBoton}>
+            <Button
+              theme={{
+                colors: { primary: '#76B39D' },
               }}
-            />
-            <TextValidator
-              style={styles.inputEmailPass}
-              placeholderTextColor="#000000"
-              required
-              label="Apellido"
-              name="apellido"
-              validators={['required']}
-              errorMessages={['* Este campo es obligatorio']}
-              placeholder="Apellido"
-              type="text"
-              value={formData.apellido}
-              onChangeText={handleChange}
-              errorStyle={{
-                container: { top: 0, left: 20, position: 'relative' },
-                text: { color: 'red' },
-                underlineValidColor: '#1E1B4D',
-                underlineInvalidColor: 'red',
-              }}
-            />
-            <TextValidator
-              style={styles.inputEmailPass}
-              placeholderTextColor="#000000"
-              required
-              label="Email"
-              name="email"
-              validators={['isEmail']}
-              errorMessages={['* El email no es válido']}
-              placeholder="Email"
-              type="text"
-              keyboardType="email-address"
-              value={formData.email}
-              onChangeText={handleChange}
-              errorStyle={{
-                container: { top: 0, left: 20, position: 'relative' },
-                text: { color: 'red' },
-                underlineValidColor: '#1E1B4D',
-                underlineInvalidColor: 'red',
-              }}
-            />
-            <TextValidator
-              style={styles.inputEmailPass}
-              placeholderTextColor="#000000"
-              required
-              name="password"
-              label="text"
-              placeholder="Contraseña"
-              secureTextEntry
-              validators={['required']}
-              errorMessages={['* Este campo es requerido']}
-              type="text"
-              value={formData.password}
-              onChangeText={handleChange}
-              errorStyle={{
-                container: { top: 0, left: 20, position: 'relative' },
-                text: { color: 'red' },
-                underlineValidColor: '#1E1B4D',
-                underlineInvalidColor: 'red',
-              }}
-            />
-            <TextValidator
-              style={styles.inputEmailPass}
-              placeholderTextColor="#000000"
-              required
-              name="repeticion"
-              label="text"
-              placeholder="Confirmar contraseña"
-              secureTextEntry
-              validators={['required']}
-              errorMessages={['* Este campo es requerido']}
-              type="text"
-              value={formData.repeticion}
-              onChangeText={handleChange}
-              errorStyle={{
-                container: { top: 0, left: 20, position: 'relative' },
-                text: { color: 'red' },
-                underlineValidColor: '#1E1B4D',
-                underlineInvalidColor: 'red',
-              }}
-            />
-          </Form>
+              style={styles.btnIngresar}
+              mode="contained"
+              title="Submit"
+              onPress={handleSubmit}
+            >
+              Guardar datos
+            </Button>
+          </View>
+          <View style={styles.contenedorError}>
+            <Text style={styles.errorDistintos}>{esDistinta}</Text>
+          </View>
+          <View style={styles.contenedorError}>
+            <Text style={styles.errorDistintos}>{mensajeError}</Text>
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
@@ -157,6 +234,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
+    top: 50,
   },
   containerTeclado: {
     flex: 1,
@@ -164,10 +242,44 @@ const styles = StyleSheet.create({
   inputEmailPass: {
     marginRight: 20,
     marginLeft: 20,
-    marginBottom: 10,
-    paddingLeft: 5,
+    marginBottom: 15,
     height: 55,
     fontSize: 18,
+    backgroundColor: '#e8e8e8',
+  },
+  contOlvidePass: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    marginBottom: 15,
+    marginRight: 25,
+  },
+  olvideMiPass: {
+    color: '#707070',
+    fontSize: 15,
+  },
+  contenedorBoton: {
+    top: 15,
+  },
+  btnIngresar: {
+    marginRight: 20,
+    marginLeft: 20,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contenedorError: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  errorDistintos: {
+    color: '#af1a1a',
+    top: 25,
+  },
+  errorPass: {
+    marginLeft: 20,
+    color: '#af1a1a',
+    top: -5,
   },
 });
 
