b08c94abcc10540bfe37c921b8ec6859c72b04bb
JuanCerutti <63431791+JuanCerutti@users.noreply.github.com>
2020-10-05 09:38:38 -0300

    notificaciones en bd

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/redux/actions/user-actions.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/redux/actions/user-actions.js
@@ -62,3 +62,60 @@ export const editarProveedores = (datos, id) => {
       });
   };
 };
+
+export const editarNotificacionesFavoritas = (datos, id) => {
+  return (dispatch, getState, { getFirestore }) => {
+    //codigo asincrono
+    const firestore = getFirestore();
+    firestore
+      .collection('usuarioConsumidor')
+      .doc(id)
+      .update({
+        notificacionesFavoritas: datos,
+      })
+      .then(() => {
+        dispatch({ type: 'EDITAR_NOTIFICACIONES' });
+      })
+      .catch((error) => {
+        dispatch({ type: 'ERROR_NOTIFICACIONES', error });
+      });
+  };
+};
+
+export const editarNotificacionesUbicacion = (datos, id) => {
+  return (dispatch, getState, { getFirestore }) => {
+    //codigo asincrono
+    const firestore = getFirestore();
+    firestore
+      .collection('usuarioConsumidor')
+      .doc(id)
+      .update({
+        notificacionesUbicacion: datos,
+      })
+      .then(() => {
+        dispatch({ type: 'EDITAR_NOTIFICACIONES' });
+      })
+      .catch((error) => {
+        dispatch({ type: 'ERROR_NOTIFICACIONES', error });
+      });
+  };
+};
+
+export const editarNotificacionesTodas = (datos, id) => {
+  return (dispatch, getState, { getFirestore }) => {
+    //codigo asincrono
+    const firestore = getFirestore();
+    firestore
+      .collection('usuarioConsumidor')
+      .doc(id)
+      .update({
+        notificacionesTodas: datos,
+      })
+      .then(() => {
+        dispatch({ type: 'EDITAR_NOTIFICACIONES' });
+      })
+      .catch((error) => {
+        dispatch({ type: 'ERROR_NOTIFICACIONES', error });
+      });
+  };
+};
