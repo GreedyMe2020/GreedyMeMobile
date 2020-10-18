d076812999ba4a83c5ff683e1645795893923c18
JuampiZablosky <44734692+JuampiZablosky@users.noreply.github.com>
2020-10-17 21:40:15 -0300

    Agrego pantalla de filtros

---
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/buscador/buscador.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SectionedMultiSelect from 'react-native-sectioned-multi-select';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { connect } from 'react-redux';
+import { editarProveedores } from '../../../redux/actions/user-actions';
+import firebaseapp from '../../../firebase/config';
+
+const items = [
+  {
+    name: 'Promoción',
+    id: 0,
+    promociones: [
+      {
+        name: '2x1',
+        id: 10,
+      },
+      {
+        name: '5x4',
+        id: 20,
+      },
+      {
+        name: '3x2',
+        id: 30,
+      },
+      {
+        name: '3x1',
+        id: 40,
+      },
+    ],
+  },
+  {
+    name: 'Descuento',
+    id: 0,
+    promociones: [
+      {
+        name: '10%',
+        id: 12,
+      },
+      {
+        name: '15%',
+        id: 22,
+      },
+      {
+        name: '25%',
+        id: 32,
+      },
+      {
+        name: '50%',
+        id: 42,
+      },
+    ],
+  },
+  {
+    name: 'Rubro',
+    id: 0,
+    promociones: [
+      {
+        name: 'Indumentaria',
+        id: 13,
+      },
+      {
+        name: 'Deporte',
+        id: 23,
+      },
+      {
+        name: 'Gastonomia',
+        id: 33,
+      },
+      {
+        name: 'Hogar',
+        id: 43,
+      },
+    ],
+  },
+];
+
+export default function BuscadorProveedores(props) {
+  const [selectedItems, setSelectedItems] = React.useState([]);
+
+  const onSelectedItemsChange = (selectedItems) => {
+    setSelectedItems(selectedItems);
+  };
+
+  return (
+    <View style={styles.container}>
+      <SectionedMultiSelect
+        items={items}
+        IconRenderer={Icon}
+        uniqueKey="id"
+        subKey="promociones"
+        selectText="Filtros..."
+        showDropDowns={true}
+        readOnlyHeadings={true}
+        onSelectedItemsChange={onSelectedItemsChange}
+        selectedItems={selectedItems}
+        confirmText="Confirmar"
+        selectedText="seleccionados"
+        searchPlaceholderText="Buscar proveedor"
+        noResultsComponent={<Text>Lo siento, no encontramos resultados.</Text>}
+        colors={{
+          primary: '#1E1B4D',
+          chipColor: '#76B39D',
+          success: '#76B39D',
+          subText: '#707070',
+        }}
+        styles={{
+          chipText: { fontSize: 16, fontWeight: 'bold' },
+          selectToggle: {
+            marginLeft: 22,
+            marginRight: 22,
+            marginBottom: 10,
+            backgroundColor: '#ececec',
+            borderRadius: 100,
+            height: 40,
+            paddingTop: 8,
+            paddingLeft: 20,
+            paddingRight: 20,
+          },
+          selectToggleText: {
+            fontSize: 17,
+          },
+          itemText: {
+            fontSize: 22,
+            marginLeft: 5,
+            paddingTop: 15,
+          },
+          toggleIcon: {
+            paddingTop: 18,
+          },
+          selectedItem: {
+            marginRight: 16,
+          },
+          subItemText: {
+            fontSize: 17,
+            marginLeft: 16,
+            marginRight: 16,
+            paddingTop: 5,
+          },
+          selectedSubItemText: {
+            fontWeight: 'bold',
+          },
+          chipsWrapper: {
+            marginLeft: 22,
+            marginRight: 22,
+          },
+          button: {
+            height: 50,
+          },
+          listContainer: {
+            marginLeft: 22,
+            marginRight: 22,
+          },
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    top: 23,
+  },
+  containerTeclado: {
+    flex: 1,
+  },
+});
--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/pages/buscar.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/pages/buscar.js
@@ -1,29 +1,79 @@
 import * as React from 'react';
-import { StyleSheet, StatusBar, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+  Text,
+  Platform,
+  Keyboard,
+  ScrollView,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
+import BuscadorProveedores from '../buscador/buscador';
+import { colors } from '../../styles/colores';
+import CardComercio from '../Inicio/card-comercio';
 
 import { connect } from 'react-redux';
 
 function Buscador() {
   return (
-    <View style={styles.contenedor}>
-      <StatusBar
-        barStyle="light-content"
-        translucent={true}
-        backgroundColor={'transparent'}
-      />
-      <View>
-        <Text>Buscar</Text>
-      </View>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.containerTeclado}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+      enabled={Platform.OS === 'ios'}
+    >
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          translucent={true}
+          backgroundColor={'transparent'}
+        />
+        <View style={styles.barraSup}>
+          <BuscadorProveedores />
+        </View>
+        <ScrollView style={styles.scroll}>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.cards}>
+              <Text style={styles.texto}>Locales</Text>
+              <CardComercio />
+            </View>
+          </TouchableWithoutFeedback>
+        </ScrollView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  contenedor: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerTeclado: {
     flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  scroll: {
+    flex: 2,
+  },
+  barraSup: {
+    marginTop: 5,
+    marginBottom: 5,
+    flex: 0.1,
+  },
+  cards: {
+    flex: 1,
+    width: '100%',
+    marginTop: 20,
+  },
+  texto: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: 18,
+    fontFamily: 'Poppins-SemiBold',
+    color: colors.darkGrey,
   },
 });
 
