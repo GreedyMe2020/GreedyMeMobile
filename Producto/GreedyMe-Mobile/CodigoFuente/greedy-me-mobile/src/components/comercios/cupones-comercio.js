0677e6aceb846b17579cc327f80811e24588f5eb
Lau <mlauri1997@gmail.com>
2020-11-02 20:47:29 -0300

    front cupon comercio mejorado

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/comercios/cupones-comercio.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/comercios/cupones-comercio.js
@@ -9,6 +9,7 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import {
+  Avatar,
   IconButton,
   Card,
   List,
@@ -21,77 +22,50 @@ import _ from 'lodash';
 import { colors } from '../../styles/colores';
 
 function CuponesComercio(props) {
-  const LeftContent = (props) => (
-    <Image
-      style={styles.image}
-      source={require('../../multimedia/adidas.jpg')}
-    />
-  );
-
-  const RightContent = (props) => (
-    <View>
-      <Title>10%</Title>
-    </View>
-  );
-
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       {/* <FlatList
         data={props.comercios}
         keyExtractor={(item) => item}
         showsVerticalScrollIndicator={false}
         renderItem={(data) => (
           <TouchableWithoutFeedback onPress={() => {}}> */}
-      <View>
-        <Card style={styles.contList}>
-          <List.Item
-            title="Club Personal"
-            titleStyle={styles.titulo}
-            description="Válido hasta el 31/10/2020"
-            style={styles.lista}
-            right={() => <Title style={styles.beneficio}>10%</Title>}
-            left={() => (
-              <Image
-                style={styles.image}
-                source={require('../../multimedia/adidas.jpg')}
-              />
-            )}
+      <View style={{ backgroundColor: colors.avatar, marginTop: 30 }}>
+        <View style={styles.card}>
+          <View style={styles.circulo} />
+          <Avatar.Image
+            style={styles.contImagen}
+            size={80}
+            source={require('../../multimedia/personal.png')}
           />
-        </Card>
-      </View>
-      <View style={styles.card}>
-        <View>
-          <Image
-            style={styles.image2}
-            source={require('../../multimedia/adidas.jpg')}
-          />
-        </View>
-        <View
-          style={{
-            height: 100,
-            width: 1,
-            backgroundColor: colors.grey,
-            marginRight: 15,
-            marginLeft: 4,
-          }}
-        />
-        <View>
-          <Title>Club Personal</Title>
-          <Title
+          <View
             style={{
-              color: colors.celeste,
-              marginTop: -2,
-              marginBottom: 5,
-              fontSize: 23,
+              height: 100,
+              width: 1,
+              backgroundColor: colors.grey,
+              marginRight: 25,
+              marginLeft: 4,
             }}
-          >
-            10% OFF
-          </Title>
-          <Text style={{ color: colors.darkGrey }}>
-            Válido hasta el 31/10/2020
-          </Text>
+          />
+          <View>
+            <Title>Club Personal</Title>
+            <Title
+              style={{
+                color: colors.naranja,
+                marginTop: -2,
+                marginBottom: 5,
+                fontSize: 23,
+              }}
+            >
+              10% OFF
+            </Title>
+            <Text style={{ color: colors.darkGrey }}>
+              Válido hasta el 31/10/2020
+            </Text>
+          </View>
         </View>
       </View>
+
       {/* </TouchableWithoutFeedback>
         )}
       /> */}
@@ -100,6 +74,10 @@ function CuponesComercio(props) {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.avatar,
+    flex: 1,
+  },
   contList: {
     marginLeft: 15,
     marginRight: 15,
@@ -110,18 +88,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    height: 80,
-    width: 80,
-    marginRight: 10,
-    borderRadius: 100,
-  },
   image2: {
     height: 80,
     width: 80,
     marginRight: 10,
     marginLeft: 10,
-    borderRadius: 100,
+    //borderRadius: 100,
+  },
+  contImagen: {
+    marginRight: 10,
+    marginLeft: 10,
+    backgroundColor: 'transparent',
   },
   titulo: {
     marginBottom: 4,
@@ -130,7 +107,9 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: colors.white,
     borderRadius: 5,
-    margin: 15,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 25,
     flexDirection: 'row',
     height: 110,
     justifyContent: 'flex-start',
@@ -142,6 +121,15 @@ const styles = StyleSheet.create({
     marginRight: 10,
     fontSize: 25,
     color: colors.naranja,
+  },
+  circulo: {
+    height: 30,
+    width: 30,
+    backgroundColor: colors.avatar,
+    borderRadius: 100,
+    position: 'absolute',
+    marginTop: 55,
+    marginLeft: -15,
   },
 });
 
