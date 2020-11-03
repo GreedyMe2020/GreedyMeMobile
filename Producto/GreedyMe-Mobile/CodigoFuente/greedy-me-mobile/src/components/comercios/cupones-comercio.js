083246a630907ea15a611ad470033067c6adb6f5
Lau <mlauri1997@gmail.com>
2020-11-03 14:24:25 -0300

    front cupones comercio

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/comercios/cupones-comercio.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/comercios/cupones-comercio.js
@@ -30,38 +30,28 @@ function CuponesComercio(props) {
         showsVerticalScrollIndicator={false}
         renderItem={(data) => (
           <TouchableWithoutFeedback onPress={() => {}}> */}
-      <View style={{ backgroundColor: colors.avatar, marginTop: 30 }}>
+      <View style={styles.content}>
         <View style={styles.card}>
-          <View style={styles.circulo} />
-          <Avatar.Image
-            style={styles.contImagen}
-            size={80}
-            source={require('../../multimedia/personal.png')}
-          />
-          <View
-            style={{
-              height: 100,
-              width: 1,
-              backgroundColor: colors.grey,
-              marginRight: 25,
-              marginLeft: 4,
-            }}
-          />
-          <View>
-            <Title>Club Personal</Title>
-            <Title
-              style={{
-                color: colors.naranja,
-                marginTop: -2,
-                marginBottom: 5,
-                fontSize: 23,
-              }}
-            >
-              10% OFF
-            </Title>
-            <Text style={{ color: colors.darkGrey }}>
-              Válido hasta el 31/10/2020
-            </Text>
+          <View style={styles.contCirculo}>
+            <View style={styles.circulo} />
+          </View>
+          <View style={styles.contCirculo2}>
+            <View style={styles.circuloEnd} />
+          </View>
+          <View style={styles.contenido}>
+            <Avatar.Image
+              style={styles.contImagen}
+              size={72}
+              source={require('../../multimedia/personal1.png')}
+            />
+            <Divider style={styles.divider} />
+            <View style={styles.texto}>
+              <Title>Club Personal</Title>
+              <Title style={styles.beneficio}>10% OFF</Title>
+              <Text style={{ color: colors.darkGrey }}>
+                Válido hasta el 31/10/2020
+              </Text>
+            </View>
           </View>
         </View>
       </View>
@@ -78,31 +68,9 @@ const styles = StyleSheet.create({
     backgroundColor: colors.avatar,
     flex: 1,
   },
-  contList: {
-    marginLeft: 15,
-    marginRight: 15,
-    marginBottom: 8,
+  content: {
+    backgroundColor: colors.avatar,
     marginTop: 30,
-  },
-  lista: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image2: {
-    height: 80,
-    width: 80,
-    marginRight: 10,
-    marginLeft: 10,
-    //borderRadius: 100,
-  },
-  contImagen: {
-    marginRight: 10,
-    marginLeft: 10,
-    backgroundColor: 'transparent',
-  },
-  titulo: {
-    marginBottom: 4,
-    fontSize: 17,
   },
   card: {
     backgroundColor: colors.white,
@@ -110,26 +78,58 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 20,
     marginBottom: 25,
-    flexDirection: 'row',
     height: 110,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
     elevation: 1,
   },
-  beneficio: {
-    alignSelf: 'center',
-    marginRight: 10,
-    fontSize: 25,
-    color: colors.naranja,
+  contCirculo: {
+    position: 'absolute',
+    justifyContent: 'center',
   },
   circulo: {
-    height: 30,
-    width: 30,
+    height: 40,
+    width: 40,
     backgroundColor: colors.avatar,
     borderRadius: 100,
+    marginLeft: -22,
+  },
+  contCirculo2: {
     position: 'absolute',
-    marginTop: 55,
-    marginLeft: -15,
+    alignSelf: 'flex-end',
+  },
+  circuloEnd: {
+    height: 40,
+    width: 40,
+    backgroundColor: colors.avatar,
+    borderRadius: 100,
+    marginRight: -22,
+  },
+  contenido: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    marginLeft: 15,
+  },
+  contImagen: {
+    marginLeft: 14,
+    marginRight: 20,
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+  },
+  divider: {
+    height: 100,
+    width: 2,
+    backgroundColor: colors.grey,
+    marginRight: 22,
+  },
+  texto: {
+    alignSelf: 'center',
+  },
+  beneficio: {
+    color: colors.naranja,
+    marginTop: -2,
+    marginBottom: 5,
+    fontSize: 23,
   },
 });
 
