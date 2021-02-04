792d68d95f3d3ea281a798dfc4ee34e725f0a599
Lau <mlauri1997@gmail.com>
2021-02-04 12:10:44 -0300

    historial de canjes

--- Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/greedypoints/greedy-historial.js
+++ Producto/GreedyMe-Mobile/CodigoFuente/greedy-me-mobile/src/components/greedypoints/greedy-historial.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { View, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
-import { Avatar, Divider, List } from 'react-native-paper';
+import { View, Text, StyleSheet } from 'react-native';
+import { DataTable } from 'react-native-paper';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { colors } from '../../styles/colores';
@@ -8,7 +8,45 @@ import { colors } from '../../styles/colores';
 function GreedyShopHistorial(props) {
   return (
     <View style={styles.container}>
-      <Text>Greedy historial de puntos</Text>
+      <DataTable>
+        <DataTable.Header style={styles.header}>
+          <DataTable.Title style={styles.headerTextF}>Fecha</DataTable.Title>
+          <DataTable.Title style={styles.headerTextP}>Producto</DataTable.Title>
+          <DataTable.Title numeric style={styles.headerTextGP}>
+            GreedyPoints
+          </DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row style={styles.row}>
+          <DataTable.Cell style={styles.fecha}>10/05/2020</DataTable.Cell>
+          <DataTable.Cell numeric style={styles.producto}>
+            Cuaderno GP
+          </DataTable.Cell>
+          <DataTable.Cell numeric style={styles.points}>
+            100
+          </DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row style={styles.row}>
+          <DataTable.Cell style={styles.fecha}>20/10/2020</DataTable.Cell>
+          <DataTable.Cell numeric style={styles.producto}>
+            Vaso térmico
+          </DataTable.Cell>
+          <DataTable.Cell numeric style={styles.points}>
+            150
+          </DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row style={styles.row}>
+          <DataTable.Cell style={styles.fecha}>28/01/2021</DataTable.Cell>
+          <DataTable.Cell numeric style={styles.producto}>
+            Mate GreedyMe
+          </DataTable.Cell>
+          <DataTable.Cell numeric style={styles.points}>
+            250
+          </DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
     </View>
   );
 }
@@ -17,6 +55,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  header: {
+    backgroundColor: colors.avatar,
+  },
+  headerTextF: {
+    justifyContent: 'flex-start',
+  },
+  headerTextP: {
+    justifyContent: 'flex-start',
+  },
+  headerTextGP: {
+    justifyContent: 'center',
+    flex: 0.5,
+  },
+  producto: {
+    justifyContent: 'flex-start',
+  },
+  points: {
+    justifyContent: 'center',
+    flex: 0.5,
   },
 });
 
